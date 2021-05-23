811519e2c245f1479c119a715607b06bb0a28f17
Mustapha Abdulmujeeb <mostalpha7@gmail.com>
2021-05-23 17:44:00 +0100

    database removed

--- server.js
+++ server.js
@@ -2,17 +2,17 @@
 
 const app = require("./startup/routes");
 const http = require("http").createServer(app);
-const db = require("./startup/db");
+// const db = require("./startup/db");
 require("dotenv").config();
 
 const port = process.env.PORT || 1000;
 
 const startServer = async() => {
-    const createDbConnection = await db();
-    if (!createDbConnection) {
-        console.log(`Error connection to mongodb databse.`);
-        return;
-    }
+    // const createDbConnection = await db();
+    // if (!createDbConnection) {
+    //     console.log(`Error connection to mongodb databse.`);
+    //     return;
+    // }
     http.listen(port, () => {
         console.log(`App is running on port ${port}`);
     });
