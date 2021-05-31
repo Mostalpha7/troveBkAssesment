586fce165cbdcd63d267783fbd77a0ed5661eb3e
Mustapha Abdulmujeeb <mostalpha7@gmail.com>
2021-05-31 21:29:54 +0100

    akdsl

--- validations/loan.validation.js
+++ validations/loan.validation.js
@@ -2,7 +2,7 @@ const Joi = require("joi");
 
 const validateTakeLoan = (data) => {
     const Schema = Joi.object({
-        loanAmount: Joi.number().positive().min(1000).required(),
+        loanAmount: Joi.number().positive().required(),
         loanPeriod: Joi.number().positive().min(6).max(12).required(),
         loanObjective: Joi.string().max(30).required(),
     });
